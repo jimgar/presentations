4b992e3ce9528e8470a23023d5b34074a1009c40
jimgar <jim@policyinpractice.co.uk>
2023-01-10 18:33:51 +0000

    Culture club webscraping

---
+++ pip-culture-club-january-2023/webscrape.R
@@ -0,0 +1,11 @@
+# Gets web pages ready for scraping.
+# This script was run Fri 6th Jan 2023.
+
+links <- c(paste0("https://www.teamblind.com/company/Twitter/reviews?page=", rep(1:30)))
+
+scraped_pages <- purrr::map(links, rvest::read_html)
+
+purrr::iwalk(
+  scraped_pages, 
+  ~ xml2::write_html(.x, paste0("pages/", if (.y < 10) paste0(0, .y) else (.y), ".html"))
+)
